9d3dfc98ea15925a36649d8801fb9cd442c254b9
sijezi <mawehelie@gmail.com>
2017-06-14 12:31:39 -0700

    4 data types

--- ByteShortInt/src/com/layth/Main.java
+++ ByteShortInt/src/com/layth/Main.java
@@ -1,16 +1,18 @@
 package com.layth;
 
 public class Main {
-
+    /*
+     * This is
+     *
+    */
     public static void main(String[] args) {
         // you may also use underscore to make number readable => -2_147_483_648
         // int has width of 32
         int minValue = -2147483648;
-        int maxValue = 2147483648;
+        int maxValue = 2147483647;
 
         // byte has a width of 8
         byte minByte  = -128;
-        byte maxByte = 128;
 
         // short has a width of 16
         short minShortType = -32767;
