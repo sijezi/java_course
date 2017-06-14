03cdfb0aec4985e620822656d7b7c36240bf01e1
sijezi <mawehelie@gmail.com>
2017-06-14 03:31:36 -0700

    first commit

---
+++ helloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * My First java program.
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        // int => a whole number without any decimal points
+        int myFirstNumber = (10 + 1)+ (2 * 5);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+    }
+}
