4efe934859c893dbee4311fe6b5197a1a985cfec
sijezi <mawehelie@gmail.com>
2017-06-28 22:17:55 -0700

    ...more data types

---
+++ CharAndBoolean/src/com/layth/Main.java
@@ -0,0 +1,25 @@
+package com.layth;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // width of 16 ( 2 bytes)
+        char myChar = '\u00A9';
+        System.out.println("Unicode Character " + myChar);
+
+        boolean myBoolean = false;
+
+        String myString = "This is a string";
+
+        System.out.println("myString is equal" + myString);
+
+        String numberString = "20.00";
+
+        numberString = numberString + "2";
+
+        System.out.println(numberString);
+
+        char registeredSymbol = '\u000AE';
+
+    }
+}
