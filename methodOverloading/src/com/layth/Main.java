5ee5fbcf99661f4c38974a8b4da9dd504b8c1805
sijezi <mawehelie@gmail.com>
2017-06-30 20:17:05 -0700

    method overloading + challenges

--- methodOverloading/src/com/layth/Main.java
+++ methodOverloading/src/com/layth/Main.java
@@ -8,7 +8,9 @@ public class Main {
         System.out.println("New Score " + newScore);
         calculateScore(75);
         calculateScore();
-    }
+
+        calcFeetAndInchesToCentimeters(6,0);
+    } // End of main
 
     public static int calculateScore(String playerName, int score) {
         System.out.println("Player " + playerName + " scored" + score + " points ");
@@ -34,4 +36,30 @@ public class Main {
         return 0;
     }
 
-}
+
+    // challenge
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if ((feet < 0) || ((inches < 0)) && ((inches > 12))) {
+            // no more processing
+            System.out.println("Invalid feeet or inches parameters");
+            return -1;
+        }
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + "  feet " + inches + " inches " + centimeters + " centimeters");
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches){
+        if(inches < 0) {
+            return -1;
+        }
+
+        double feet =  (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " is equal to " + feet " feet and " + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters();
+    }
+
+
+} // End of Class
