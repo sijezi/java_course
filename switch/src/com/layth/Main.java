88d287a8b2969b5f8100ce999dcd77d048b378ab
sijezi <mawehelie@gmail.com>
2017-07-06 01:12:26 -0700

    challenge

--- switch/src/com/layth/Main.java
+++ switch/src/com/layth/Main.java
@@ -12,7 +12,7 @@ public class Main {
 //        }else {
 //            System.out.println("Value is 1 or 2");
 //        }
-
+        switchTest();
         int switchValue = 1;
         /* only tests switchValue, it is only good if your testing the same variables
         * byte short char and int
@@ -31,6 +31,24 @@ public class Main {
 
             default:
                 System.out.println("Value was not 1 or 2");
+        }
+    }
+
+    public static void switchTest() {
+        char charValue = 'A';
+        switch(charValue) {
+            case 'A':
+                System.out.println("A was found");
+                break;
+            case 'B':
+                System.out.println("B was found");
+                break;
+            case 'C':case 'D':case 'E':
+                System.out.println(charValue + " was found");
+                break;
+            default:
+                System.out.println("Could not find A, B, C, D, or E");
+                break;
         }
     }
 }
